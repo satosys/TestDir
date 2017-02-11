ec35bdfdf99071b18d1b0653caa613796f156ff2
satosys <satya9pinu@gmail.com>
2017-02-11 02:23:14 -0500

    String tutorial

--- Stings.py
+++ Stings.py
@@ -1,9 +1,8 @@
-print ('This is also a string')
+print('This is also a string')
 
 print(len('Satyajit Das you should leann Data Science within 2 Months of time'))
 s = 'Hello World'
 print(s[0])
-
 
 print('{} {}'.format('one', 'two'))
 
@@ -14,3 +13,5 @@ print('{:_>10}'.format('test'))
 print('{:_^10}'.format('test'))
 
 print('This a test value:''{x:}'.format(x='test'))
+
+print(f'{3.456678:.5f}')
