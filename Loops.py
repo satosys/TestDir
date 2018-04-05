1efb26d3877d005a4787a575c23724094cb52f9a
satosys <satya9pinu@gmail.com>
2018-04-05 07:08:04 -0400

    ok.

--- Loops.py
+++ Loops.py
@@ -1,7 +1,7 @@
 from builtins import print
 
 x = 0
-
 while x == 0:
     print('i am within while loop')
     x += 1
+range(1,7)
---
+++ Test.py
@@ -0,0 +1,18 @@
+from sys import argv
+
+sys.argv[0] ='first'
+sys.argv[1] ='first'
+sys.argv[2] ='first'
+sys.argv[3] ='first'
+
+script, first, second, third = argv
+
+print ('The script is called ', script)
+
+print ('The script is called ', first)
+
+print ('The script is called ', second)
+
+print ('The script is called ', third)
+
+print ('The script is called ', third)
